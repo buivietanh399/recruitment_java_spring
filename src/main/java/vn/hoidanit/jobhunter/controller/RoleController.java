e87306f44170fc1a70fed0713d0b566248728590
Viet Anh <buivietanh399@gmail.com>
2023-06-22 21:09:00 +0700

    update module Permission, Role, Resume

--- src/main/java/vn/hoidanit/jobhunter/controller/RoleController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/RoleController.java
@@ -54,7 +54,7 @@ public class RoleController {
         // tại");
         // }
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.roleService.update(r));
+        return ResponseEntity.ok().body(this.roleService.update(r));
     }
 
     @DeleteMapping("/roles/{id}")
@@ -74,6 +74,18 @@ public class RoleController {
             @Filter Specification<Role> spec, Pageable pageable) {
 
         return ResponseEntity.ok(this.roleService.getRoles(spec, pageable));
+    }
+
+    @GetMapping("/roles/{id}")
+    @ApiMessage("Fetch role by id")
+    public ResponseEntity<Role> getById(@PathVariable("id") long id) throws IdInvalidException {
+
+        Role role = this.roleService.fetchById(id);
+        if (role == null) {
+            throw new IdInvalidException("Resume với id = " + id + " không tồn tại");
+        }
+
+        return ResponseEntity.ok().body(role);
     }
 
 }
